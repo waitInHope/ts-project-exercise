7ef13ea8650581dc1667d22006bc41e8b09c7091
waitInHope <1064600154@qq.com>
2022-06-21 20:59:38 +0800

    feat: 判别分布式条件类型和非分布式条件类型

--- example.ts
+++ example.ts
@@ -1,5 +1,5 @@
 
-import { Pick } from './utilsType'
+import { Pick, MyExclude, IsStringArray, IsStringArrayNone1 } from './utilsType'
 
 interface People {
     name: string;
@@ -8,5 +8,10 @@ interface People {
 }
 
 type PeopleName = Pick<People, 'name'>;
-
 type PeopleName1 = Omit<People, 'age'|'height'>
+
+type t4 = Exclude<'a'|'b'|'c', 'a'|'b'>;
+type t5 = MyExclude<'a'|'b'|'c', 'a'|'b'>;
+
+type t6 = IsStringArrayNone1<1 | 'a'>;
+type t7 = IsStringArray<[1]|['a']>;
--- utilsType.ts
+++ utilsType.ts
@@ -7,3 +7,17 @@ export type Pick<T, P extends keyof T> = {
 export type Omit<T, E extends string|number|symbol> = {
     [k in Exclude<keyof T, E>]: T[k]
 }
+
+
+/**
+ * 分布式条件类型 与 非分布式条件类型
+ */
+export type MyExclude<T, U> = T extends U ? never : T;
+
+// 分布式条件类型
+export type IsStringArray<T> = T extends string[] ? true : false;
+
+// 非分布式条件类型 使用[T]、T[]、Promise<T>包裹的条件类型都是非分布式
+export type IsStringArrayNone1<T> = T[] extends string[] ? true : false;
+export type IsStringArrayNone2<T> = [T] extends [string] ? true : false;
+export type IsStringArrayNone3<T> = Promise<T> extends Promise<string> ? true : false;
